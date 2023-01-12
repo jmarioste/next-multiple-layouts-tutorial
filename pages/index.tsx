49bd0c7da04c8958f761547e4f799823af509bff
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2023-01-12 15:40:05 +0800

    Change text on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,9 @@ const HomePage: NextPage = () => {
     <div className="container">
       <div className="grid place-content-center min-h-screen">
         <div className="flex flex-col items-center gap-4">
-          <h1 className="text-4xl my-8">Welcome to NextJS DaisyUI Starter</h1>
+          <h1 className="text-4xl my-8">
+            Welcome to NextJS Multiple Layouts Tutorial
+          </h1>
         </div>
       </div>
     </div>
